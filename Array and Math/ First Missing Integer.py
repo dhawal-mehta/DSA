bb3e9e82012eae11792fa4129ba00f572b0c75ae
dhawal-mehta <dhawalmehta144@gmail.com>
2021-09-14 05:33:41 +0530

    added codes in Array and maths

---
+++ Array and Math/ First Missing Integer.py
@@ -0,0 +1,151 @@
+"""
+First Missing Integer
+
+Problem Description
+
+Given an unsorted integer array A of size N. Find the first missing positive integer.
+
+Note: Your algorithm should run in O(n) time and use constant space.
+
+
+
+Problem Constraints
+
+    1 <= N <= 1000000
+
+    -109 <= A[i] <= 109
+
+
+
+Input Format
+
+First argument is an integer array A.
+
+
+Output Format
+
+Return an integer denoting the first missing positive integer.
+
+
+Example Input
+
+    Input 1:
+
+    [1, 2, 0]
+
+    Input 2:
+
+    [3, 4, -1, 1]
+
+    Input 3:
+
+    [-8, -7, -6]
+
+
+
+Example Output
+
+    Output 1:
+
+    3
+
+    Output 2:
+
+    2
+
+    Output 3:
+
+    1
+
+
+
+Example Explanation
+
+Explanation 1:
+
+    A = [1, 2, 0]
+    First positive integer missing from the array is 3.
+
+"""
+
+""" Hint1
+To simply solve this problem, search all positive integers, starting from 1 in the given array. We may have to search at most n+1 numbers in the given array. So this solution takes O(n^2) in worst case.
+
+We can use sorting to solve it in lesser time complexity. We can sort the array in O(nLogn) time.
+
+Once the array is sorted, then a linear scan of the array is all that remains to be done.
+
+So this approach takes O(nLogn + n) time which is O(nLogn).
+
+We can also use hashing. We can build a hash table of all positive elements in the given array.
+
+Once the hash table is built, all positive integers, starting from 1 can be searched here. As soon as we find a number which is not there in the hash table, we return it.
+
+Approximately, this approach may take O(n) time, but it requires O(n) extra space.
+
+But what we are really looking for is a O(n) time, O(1) space solution.
+
+Note that ( N being the size of the array ), A[i]<=0 and A[i]>N are not the numbers you are looking for because the missing positive integer will be in the range [1, N+1].
+
+The answer will be N+1 only if all of the elements of the array are exact one occurrence of [1, N].
+
+If using extra space was an option, creating buckets would have been an easy solution.
+
+Creating an array of size N initialized to 0, for every value A[i] which lies in the range [1, N], we would have incremented its count in the array. Consequently, we would traverse the array to find the first array position with value 0, hence finding our answer.
+
+However, since we are not allowed buckets, can we use the existing array as bucket somehow?
+
+"""
+
+
+""" solution Approach
+
+Note: numbers A[i]<=0 and A[i]>N ( N being the size of the array ) is not important to us since the missing positive integer will be in the range [1, N+1].
+
+The answer will be N+1 only if all of the elements of the array are exact one occurrence of [1, N].
+
+Creating buckets would have been an easy solution if using extra space was allowed.
+
+An array of size N initialized to 0 would have been created.
+
+For every value A[i] which lies in the range [1, N], its count in the array would have been incremented.
+
+Finally, traversing the array would help to find the first array position with value 0 and that will be our answer.
+However, usage of buckets is not allowed, can we use the existing array as bucket somehow?
+
+Now, since additional space is not allowed either, the given array itself needs to be used to track it.
+
+It may be helpful to use the fact that the size of the set we are looking to track is [1, N]
+
+which happens to be the same size as the size of the array.
+
+This means we can use the array to track these elements.
+
+We traverse the array and if A[i] is in [1,N] range, we try to put in the index of same value in the array.
+
+
+"""
+
+""" Accepted solution """
+# @param A : list of integers
+# @return an integer
+
+def firstMissingPositive(self, A):
+    # partition
+    n=len(A)
+    end=0
+    for i in range(n):
+        if A[i] > 0:
+            A[i],A[end] = A[end],A[i]
+            end+=1
+    
+    # negative for visit
+    for i in range(end):
+        if abs(A[i]) <= end:
+            A[abs(A[i])-1]*=-1
+
+    for i in range(end):
+        if A[i] > 0:
+            return i+1
+    return end+1
+
---
+++ Array and Math/N by 3 Repeat number.py
@@ -0,0 +1,97 @@
+""" 
+N/3 Repeat Number
+
+Problem Description
+You're given a read only array of n integers. Find out if any integer occurs more than n/3 times in the array in linear time and constant additional space.
+
+If so, return the integer. If not, return -1.
+
+If there are multiple solutions, return any one.
+
+Example:
+
+Input: [1 2 3 1 1]
+Output: 1 
+1 occurs 3 times which is more than 5/3 times.
+
+"""
+"""
+Hint 1
+It works to simply pick all elements one by one. For every picked element, count its occurrences by traversing the array.
+If count becomes more than n/3, then print the element. Time Complexity of this method would be O(n^2).
+
+A better solution is to use sorting.
+
+First, sort all elements using a O(nLogn) algorithm. All required elements in a linear scan of array can be found once the array is sorted.
+
+So overall, time complexity of this method is O(nLogn) + O(n) which is O(nLogn).
+
+However, a linear solution is needed here.
+
+Note: if at any instance, you have three distinct elements from the array, if you remove them from the array, your answer does not change.
+
+Try to base your solution idea on the above fact.
+
+Would it help to maintain two elements from array with their count as you traversed the array ?
+
+"""
+
+"""
+solution approach
+
+It is important to note that if at a given time, you have 3 distinct element from the array, if you remove them from the array, your answer does not change.
+
+Assume that we maintain 2 elements with their counts as we traverse along the array.
+
+When we encounter a new element, there are 3 cases possible :
+
+    We don’t have 2 elements yet. So add this to the list with count as 1.
+
+    This element is different from the existing 2 elements. As we said before, we have 3 distinct numbers now. Removing them does not change the answer. So decrement 1 from count of 2 existing elements. If their count falls to 0, obviously its not a part of 2 elements anymore.
+
+    The new element is same as one of the 2 elements. Increment the count of that element.
+
+Consequently, the answer will be one of the 2 elements left behind. If they are not the answer, then there is no element with count > N / 3.
+
+"""
+
+""" accepted solution """
+
+# @param A : tuple of integers
+# @return an integer
+def repeatedNumber(self, A):
+    n=len(A)
+    first_major=0
+    second_major=0
+    first_count=0
+    second_count=0
+    
+    for i in range(n):
+        
+        if A[first_major] == A[i]:
+            first_count+=1
+        elif A[second_major] == A[i]:
+            second_count+=1
+        elif first_count == 0:
+            first_major = i
+            first_count+=1
+        elif second_count == 0:
+            second_major = i
+            second_count += 1
+        else:
+            first_count-=1
+            second_count-=1
+    first_count = 0
+    second_count = 0
+    for i in range(n):
+        if A[i] == A[first_major]:
+            first_count+=1
+        elif A[i] == A[second_major]:
+            second_count+=1
+    if first_count > n//3:
+        return A[first_major]
+    elif second_count > n//3:
+        return A[second_major]
+    else:
+        return -1
+            
