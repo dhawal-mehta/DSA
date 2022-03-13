a92d0a5f4b025c588ce259f52a0ca90bfb05415e
dhawal-mehta <dhawalmehta144@gmail.com>
2022-03-13 06:59:57 +0530

    complted DP trees only one question

---
+++ Dynamic programming/DP on trees/Maximum path sum from any node.py
@@ -0,0 +1,175 @@
+"""
+Maximum path sum from any node
+
+
+Given a binary tree, the task is to find the maximum path sum. The path may start and end at any node in the tree.
+
+Example 1:
+
+Input:
+     10
+    /  \
+   2   -25
+  / \  /  \
+ 20 1  3  4
+Output: 32
+Explanation: Path in the given tree goes
+like 10 , 2 , 20 which gives the max
+sum as 32.
+
+Example 2:
+
+Input:
+     10
+   /    \
+  2      5
+          \
+          -2
+Output: 17
+Explanation: Path in the given tree goes
+like 2 , 10 , 5 which gives the max sum
+as 17.
+
+Your Task:
+You don't need to take input or print anything. Your task is to complete the function findMaxSum() that takes root as input and returns max sum between any two nodes in the given Binary Tree.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Height of the Tree).
+
+Constraints:
+1 ≤ Number of nodes ≤ 103
+1 ≤ |Data on node| ≤ 104
+"""
+"""
+Hint 1
+For each node there can be four ways that the max path goes through the node:
+1. Node only
+2. Max path through Left Child + Node
+3. Max path through Right Child + Node
+4. Max path through Left Child + Node + Max path through Right Child
+
+The idea is to keep trace of four paths and pick up the max one in the end. An important thing to note is, root of every subtree needs to return maximum path sum such that at most one child of root is involved. This is needed for parent function call.
+"""
+#User function Template for python3
+
+
+'''
+class Node:
+    def __init__(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+
+class Solution:
+    #Function to return maximum path sum from any node in a tree.
+    def __init__(self):
+        self.ans = float('-inf')
+        
+        
+    def util(self, root):
+        if root==None:
+            return 0
+        
+        lt = self.util( root.left )
+        rt = self.util( root.right )
+        
+        temp = max(max(lt,rt) + root.data, root.data)
+        
+        temp2 =  max(temp, lt+rt+root.data)
+        
+        self.ans = max(self.ans, temp2)
+        
+        return temp
+        
+    def findMaxSum(self, root): 
+        #Your code here
+        self.util(root)
+        return self.ans
+        
+        
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+#Contributed by Suman Rana
+import sys
+sys.setrecursionlimit(100000)
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+    
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+
+
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        root=buildTree(input())
+        ob = Solution()
+        print(ob.findMaxSum(root))
+        
+        
+
+# } Driver Code Ends
